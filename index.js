0d5b5f6e9189753f6056bf4bc227c6edd832ae44
Renebwoi <demiurgerene@gmail.com>
2024-08-29 13:22:22 +0100

    changing port

--- index.js
+++ index.js
@@ -22,7 +22,8 @@ const app = express();
 app.use(cors())
 app.use(express.json());
 app.use('/api', routes)
+const port = process.env.PORT || 3003
 
-app.listen(3003, () => {
-    console.log(`Server Started at ${3003}`)
+app.listen(port, () => {
+    console.log(`Server Started at ${port}`)
 })
